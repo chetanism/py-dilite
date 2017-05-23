4a9b0e62686bcef633bfa7c17b481be26487bb47
chetan verma <verma.chetan@gmail.com>
2017-05-23 20:07:29 +0530

    v0.0.2

--- setup.py
+++ setup.py
@@ -2,13 +2,22 @@ from distutils.core import setup
 
 setup(
     name='dilite',
-    packages=['dilite'],
-    version='0.0.1',
+    version='0.0.2',
     description='An extremely light weight DI container',
+    long_description='An extremely light weight DI container. The goal is to keep Dilite light ' +
+                     'enough that it seems logical to have multiple instances of Dilite wired up ' +
+                     'together in a single application.',
+    url='https://github.com/chetanism/py-dilite',
     author='Chetan Verma',
     author_email='verma.chetan@gmail.com',
-    url='https://github.com/chetanism/py-dilite',
-    download_url='https://github.com/peterldowns/chetanism/py-dilite/0.1.tar.gz',
-    keywords=['di', 'ioc', 'dependency-injection', 'dependency injection'],
-    classifiers=[],
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development'
+    ],
+    keywords='di ioc dependency-injection dependency injection',
+    packages=['dilite']
 )
